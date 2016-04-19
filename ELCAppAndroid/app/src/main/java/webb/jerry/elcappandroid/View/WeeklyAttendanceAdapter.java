054fab036265839ca523e580173fa7794fe2bdae
Jerry Webb <jerryweb@usc.edu>
2016-04-19 00:31:59 -0700

    pushing

---
+++ ELCAppAndroid/app/src/main/java/webb/jerry/elcappandroid/View/WeeklyAttendanceAdapter.java
@@ -0,0 +1,12 @@
+package webb.jerry.elcappandroid.View;
+
+import android.widget.ArrayAdapter;
+
+import webb.jerry.elcappandroid.Model.Course;
+
+/**
+ * Created by LJ on 4/18/16.
+ */
+public class WeeklyAttendanceAdapter extends ArrayAdapter<Course> {
+
+}
